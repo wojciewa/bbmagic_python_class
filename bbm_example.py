f4f6ae8a307dd33b46810931197a44b6013a6478
Gabriel <gabriel.zima@gmail.com>
2018-07-03 23:51:22 +0200

    Update bbm_example.py

--- bbm_example.py
+++ bbm_example.py
@@ -10,7 +10,7 @@ i = bbm.bbm_bt_open(17)
 while True:
     js = bbm.bbm_bt_read_json()
     if js['result'] > 0 :
-        print js['mac'] + " | " + str(js['type'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag'])
+        print js['mac'] + " | " + str(js['type']) + " | " + str(js['adv_time'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag'])
         #print js
     elif js['result'] == -1 :
         i = bbm.bbm_bt_close()
