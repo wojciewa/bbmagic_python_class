0a708987711f66a1dc32d0529f8df77e686bd1f0
Gabriel <gabriel.zima@gmail.com>
2018-07-04 10:01:52 +0200

    Update bbm_example.py

--- bbm_example.py
+++ bbm_example.py
@@ -10,8 +10,15 @@ i = bbm.bbm_bt_open(17)
 while True:
     js = bbm.bbm_bt_read_json()
     if js['result'] > 0 :
+        ### example print to console
         print js['mac'] + " | " + str(js['type']) + " | " + str(js['adv_time'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag'])
-        #print js
+        ### example print object (json) to console
+        # print js
+        ### example send to Domoticz
+        # idx = 123
+        # flood_flag = 'Off' if js['flood_flag'] else 'On'
+        # r = requests.get('https://<ip_domoticz>:<domoticz_port>/json.htm?type=command&param=udevice&idx={0}&nvalue=0&svalue={1}'.format(idx, flood_flag), auth=('user', 'pass'))
+        # pritn r.status_code
     elif js['result'] == -1 :
         i = bbm.bbm_bt_close()
         exit(0)
