46c0e5b8c84730d4397b5c39d732571de299df5a
Gabriel <gabriel.zima@gmail.com>
2018-07-04 11:57:50 +0200

    Update bbmlib2dynamic.sh

--- bbmlib2dynamic.sh
+++ bbmlib2dynamic.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+## prepare library bbmagic_lib_x.x.a to dynamic library bbmagic_lib_x.x.so
+
 libv="1.2"
-## prepare dynamic library bbmagic_lib to dynamic library
+
 wget http://bbmagic.net/download/bin/bbmagic_lib_$libv.tar.gz
 tar -zxvf bbmagic_lib_$libv.tar.gz
 cd bbmagic_lib_$libv
@@ -9,5 +11,7 @@ wget http://bbmagic.net/download/bin/libbluetooth.a
 gcc -shared -o bbmagic_lib_$libv.so bbmagic_lib_$libv.o libbluetooth.a
 rm bbmagic_lib_$libv.a bbmagic_lib_$libv.o libbluetooth.a
 cd ..
+cp ./bbmagic_lib_$libv/bbmagic_lib_$libv.so bbmagic_lib_$libv.so
 rm bbmagic_lib_$libv.tar.gz
+rm -r ./bbmagic_lib_$libv
 ls
