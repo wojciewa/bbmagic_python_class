8b0139a0017cfea24293338d4d2d659baaf5fa45
Gabriel <gabriel.zima@gmail.com>
2018-07-04 09:45:45 +0200

    Update bbm_class.py

--- bbm_class.py
+++ bbm_class.py
@@ -57,7 +57,6 @@ class BBMagic:
         self.BBLIB_FRAME_SIZE = 23
         self.BBM_BT_ADDR_SIZE = 6
         self.BBMAGIC_VCC_DIVIDER = 71
-
         self.bbm_buf = (c_byte * self.BBLIB_FRAME_SIZE)()
 
     #Function: open bt hci and starts bt scanning
@@ -90,42 +89,45 @@ class BBMagic:
 
         d = dict()
         d['result'] = i
-        d['raw'] = self.bbm_buf
 
-        worktime = self.BBMGIC_DEVICE_WORKTIME_3 * 0xFFFFFF
-        worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_2 * 0xFFFF
-        worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_1 * 0xFF
-        worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_0
-        d['worktime'] = worktime
+        if i < 1 :
+            d['raw'] = self.bbm_buf
+            
+            worktime = self.BBMGIC_DEVICE_WORKTIME_3 * 0xFFFFFF
+            worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_2 * 0xFFFF
+            worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_1 * 0xFF
+            worktime = worktime + self.BBMGIC_DEVICE_WORKTIME_0
+            d['worktime'] = worktime
 
-        device_mac = ''
-        for i in range(self.BBMAGIC_DEVICE_ADDR_5, self.BBMAGIC_DEVICE_ADDR_0 + 1) :
-            device_mac = device_mac + format(bbm_buf_bytes[i], 'x')
-        d['mac'] = device_mac
-        d['rssi'] = self.bbm_buf[self.BBMAGIC_DEVICE_RSSI]
-        d['v_supl'] = "{:4.2f}".format(float(bbm_buf_bytes[self.BBMAGIC_DEVICE_V_SUP]) / self.BBMAGIC_VCC_DIVIDER)
-        d['adv_time'] = self.bbm_buf[self.BBMAGIC_DEVICE_ADV_TIME]
-        d['dio_state'] = self.bbm_buf[self.BBMAGIC_DEVICE_DIO_STATE]
+            device_mac = ''
+            for i in range(self.BBMAGIC_DEVICE_ADDR_5, self.BBMAGIC_DEVICE_ADDR_0 + 1) :
+                device_mac = device_mac + format(bbm_buf_bytes[i], 'x')
+            d['mac'] = device_mac
+            
+            d['rssi'] = self.bbm_buf[self.BBMAGIC_DEVICE_RSSI]
+            d['v_supl'] = "{:4.2f}".format(float(bbm_buf_bytes[self.BBMAGIC_DEVICE_V_SUP]) / self.BBMAGIC_VCC_DIVIDER)
+            d['adv_time'] = self.bbm_buf[self.BBMAGIC_DEVICE_ADV_TIME]
+            d['dio_state'] = self.bbm_buf[self.BBMAGIC_DEVICE_DIO_STATE]
 
-        device_type = self.bbm_buf[self.BBMAGIC_DEVICE_TYPE]
-        d['type'] = device_type
+            device_type = self.bbm_buf[self.BBMAGIC_DEVICE_TYPE]
+            d['type'] = device_type
 
-        if device_type == self.BBMAGIC_M_FLOOD :
-            d['flood_alert_flag'] = self.bbm_buf[self.BBM_FLOOD_ALERT_FLAG]
-            d['flood_chip_temp'] = self.bbm_buf[self.BBM_FLOOD_CHIP_TEMP]
-            d['flood_firm_1'] = self.bbm_buf[self.BBM_FLOOD_FIRM_1]
-            d['flood_firm_0'] = self.bbm_buf[self.BBM_FLOOD_FIRM_0]
-        elif device_type == self.BBMAGIC_M_MOTION :
-            d['motion_alert_flag'] = self.bbm_buf[self.BBM_MOTION_MOTION_ALERTS]
-            d['motion_light'] = self.bbm_buf[self.BBM_MOTION_LIGHT]
-        elif device_type == self.BBMAGIC_M_BUTTON :
-            d['button_function'] = self.bbm_buf[self.BBM_BUTTON_BUTTON_FUNCTION]
-            d['button_cfg_pins'] = self.bbm_buf[self.BBM_BUTTON_CFG_PINS]
-            d['button_chip_temp'] = self.bbm_buf[self.BBM_BUTTON_CHIP_TEMP]
-            d['button_light'] = self.bbm_buf[self.BBM_BUTTON_LIGHT]
-        elif device_type == self.BBMAGIC_M_METEO :
-            d['meteo_hum'] = self.bbm_buf[self.BBM_METEO_HUM]
-            #d['meteo_temp'] = self.bbm_buf[self.BBM_METEO_TEMPER_MSB] #self.bbm_buf[self.BBM_METEO_TEMPER_LSB]
-            d['meteo_light'] = self.bbm_buf[self.BBM_METEO_LIGHT]
+            if device_type == self.BBMAGIC_M_FLOOD :
+                d['flood_alert_flag'] = self.bbm_buf[self.BBM_FLOOD_ALERT_FLAG]
+                d['flood_chip_temp'] = self.bbm_buf[self.BBM_FLOOD_CHIP_TEMP]
+                d['flood_firm_1'] = self.bbm_buf[self.BBM_FLOOD_FIRM_1]
+                d['flood_firm_0'] = self.bbm_buf[self.BBM_FLOOD_FIRM_0]
+            elif device_type == self.BBMAGIC_M_MOTION :
+                d['motion_alert_flag'] = self.bbm_buf[self.BBM_MOTION_MOTION_ALERTS]
+                d['motion_light'] = self.bbm_buf[self.BBM_MOTION_LIGHT]
+            elif device_type == self.BBMAGIC_M_BUTTON :
+                d['button_function'] = self.bbm_buf[self.BBM_BUTTON_BUTTON_FUNCTION]
+                d['button_cfg_pins'] = self.bbm_buf[self.BBM_BUTTON_CFG_PINS]
+                d['button_chip_temp'] = self.bbm_buf[self.BBM_BUTTON_CHIP_TEMP]
+                d['button_light'] = self.bbm_buf[self.BBM_BUTTON_LIGHT]
+            elif device_type == self.BBMAGIC_M_METEO :
+                d['meteo_hum'] = self.bbm_buf[self.BBM_METEO_HUM]
+                #d['meteo_temp'] = self.bbm_buf[self.BBM_METEO_TEMPER_MSB] #self.bbm_buf[self.BBM_METEO_TEMPER_LSB]
+                d['meteo_light'] = self.bbm_buf[self.BBM_METEO_LIGHT]
 
         return d
