92e5fcf90c5aaa858419fedb45016720781d4397
Gabriel <gabriel.zima@gmail.com>
2018-07-05 18:14:38 +0200

    Add descriptions to library key functions

--- bbm_example.py
+++ bbm_example.py
@@ -1,4 +1,4 @@
-from bbm_class import BBMagic
+from bbm_class import BBMagic # import python BBMagic class 
 from time import sleep
 
 print("")
@@ -6,17 +6,17 @@ print("    ***  BBMagic example   ***")
 print("    *** hit Ctrl+C to stop ***")
 print("")
 
-bbm = BBMagic()
-i = bbm.bbm_bt_lib_version()
-print("BBMagic library version is {}".format(i))
+bbm = BBMagic() # initialize BBMagic class
+i = bbm.bbm_bt_lib_version() # get BBMagic library version
+print("BBMagic library version is {}".format(i)
 
-i = bbm.bbm_bt_open(17)
+i = bbm.bbm_bt_open(17) # open bt hci and starts bt scanning
 if i != 0:
     print("some errors occured")
     exit(0)
 
 while True:
-    js = bbm.bbm_bt_read_json()
+    js = bbm.bbm_bt_read_json() # get object from scanning line
     if js['result'] > 0 :
         ### example print to console
         print(js['mac'] + " | " + str(js['type']) + " | " + str(js['adv_time'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag']))
@@ -29,6 +29,6 @@ while True:
         #     r = requests.get('https://<ip_domoticz>:<domoticz_port>/json.htm?type=command&param=udevice&idx={0}&nvalue=0&svalue={1}'.format(idx, flood_flag), auth=('user', 'pass'))
         #     print(r.status_code)
     elif js['result'] == -1 :
-        i = bbm.bbm_bt_close()
+        i = bbm.bbm_bt_close() # tops bt scanning and closes bt hci
         exit(0)
     sleep(1)
