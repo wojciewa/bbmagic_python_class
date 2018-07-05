007a65555818b7cec0ad9761de48cb8a2655a641
Gabriel <gabriel.zima@gmail.com>
2018-07-05 09:11:00 +0200

    change to compatibile with python 3

--- bbm_example.py
+++ bbm_example.py
@@ -1,14 +1,14 @@
 from bbm_class import BBMagic
 from time import sleep
 
-print
-print "    ***  BBMagic example   ***"
-print "    *** hit Ctrl+C to stop ***"
-print
+print("")
+print("    ***  BBMagic example   ***")
+print("    *** hit Ctrl+C to stop ***")
+print("")
 
 bbm = BBMagic()
 i = bbm.bbm_bt_lib_version()
-print "BBMagic library version is {0}".format(i)
+print("BBMagic library version is {}".format(i))
 
 i = bbm.bbm_bt_open(17)
 
@@ -16,15 +16,15 @@ while True:
     js = bbm.bbm_bt_read_json()
     if js['result'] > 0 :
         ### example print to console
-        print js['mac'] + " | " + str(js['type']) + " | " + str(js['adv_time'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag'])
+        print(js['mac'] + " | " + str(js['type']) + " | " + str(js['adv_time'])  + " | " + str(js['v_supl']) + "V | " + str(js['rssi']) + "dBm | " + str(js['flood_alert_flag']))
         ### example print object (json) to console
-        # print js
+        # print(js)
         ### example send to Domoticz dummy sensor
         # if js['mac'] == 'C527FECE713C' : # check mac for BBMagic flood device
         #     idx = 123
         #     flood_flag = 'Off' if js['flood_flag'] else 'On'
         #     r = requests.get('https://<ip_domoticz>:<domoticz_port>/json.htm?type=command&param=udevice&idx={0}&nvalue=0&svalue={1}'.format(idx, flood_flag), auth=('user', 'pass'))
-        #     print r.status_code
+        #     print(r.status_code)
     elif js['result'] == -1 :
         i = bbm.bbm_bt_close()
         exit(0)
