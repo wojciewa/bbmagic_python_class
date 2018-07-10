ba69f01ec6cce1770e350c16fef08d0254263b85
Gabriel Zima <gabriel.zima@g2a.com>
2018-07-10 07:42:21 +0200

    Clean code in example

--- bbm_example.py
+++ bbm_example.py
@@ -16,13 +16,16 @@ while True:
     js = bbm.bbm_bt_read_json()
     if js['result'] > 0 :
          print(js)
+
         ### example send to Domoticz dummy sensor
-        # if js['mac'] == 'C527FECE713C' : # check mac for BBMagic flood device
+        # if js[type_name] == 'bbm_flood' && js['mac'] == 'C527FECE713C' : # check mac for BBMagic flood device
         #     idx = 123
         #     flood_flag = 'Off' if js['flood_flag'] else 'On'
         #     r = requests.get('https://<ip_domoticz>:<domoticz_port>/json.htm?type=command&param=udevice&idx={0}&nvalue=0&svalue={1}'.format(idx, flood_flag), auth=('user', 'pass'))
         #     print(r.status_code)
+        #     print(r.text) 
+
     elif js['result'] == -1 :
         i = bbm.bbm_bt_close()
         exit(0)
-    sleep(0.1)
+    sleep(5)
